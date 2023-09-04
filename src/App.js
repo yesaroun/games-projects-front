6031da60c52130cf5730a0a31b01b0496955a1b4
yesaroun <akor1@naver.com>
2023-09-05 00:49:08 +0900

    [feat] basic format

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 function App() {
   return (
     <div className="App">
-      <div id="wrap" class="wrap">
-
+      <div className="wrap">
+        <div className="window">
+          <div className="title-bar">
+            title!!!
+            <div>
+              <button aria-label="Minimize"></button>
+              <button aria-label="Maximize"></button>
+              <button aria-label="Close"></button>
+            </div>
+          </div>
+          <div className="window-body">This is Body</div>
+        </div>
       </div>
     </div>
   );
